1b3e718f2ba618de87adafaabf82d0a42ab5a74a
Ananda <nandaprana76@gmail.com>
2017-11-23 13:20:57 +0700

    Update InputBukuContext.java

--- InputBukuContext.java
+++ InputBukuContext.java
@@ -9,27 +9,47 @@ import cucumber.api.PendingException;
 import cucumber.api.java.bm.Maka;
 import cucumber.api.java.id.Dengan;
 import cucumber.api.java.id.Ketika;
+import java.util.Scanner;
 
 /**
  *
- * @author Bagas Bowo
+ * @author ananda
  */
 public class InputBukuContext {
  @Dengan("^data buku baru yang akan dimasukkan dan tidak duplikat$")
 public void data_buku_baru_yang_akan_dimasukkan_dan_tidak_duplikat() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
+    Scanner scan = new Scanner(System.in);
+    int data_buku=scan.nextInt();
+    String judulbuku [ ]=new String[data_buku];
+    String Namapengarang [ ]=new String[data_buku];
+    String TahunTerbit [ ]=new String[data_buku];
     //throw new PendingException();
 }
 
 @Ketika("^memasukkan data buku$")
 public void memasukkan_data_buku() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
+    for(int i=1;i<=data_buku;i++){
+            System.out.println("Data buku ke -"+i);
+            System.out.println("Judul buku = ");
+            judulbuku[i-1]=scan.next();
+            System.out.println("Nama Pengarang = ");
+            Namapengarang[i-1]=scan.next();
+            System.out.println("Tahun Terbit = ");
+            TahunTerbit[i-1]=scan.next();
+            System.out.println("");
+            System.out.println("");
+        }
     //throw new PendingException();
 } 
 
 @Maka("^respon \"([^\"]*)\"$")
 public void respon(String arg1) throws Throwable {
-  // Write code here that turns the phrase above into concrete actions
+        System.out.println("Data Buku");
+        System.out.println("");
+        for(int i=1;i<=data_buku;i++){
+            System.out.println(i+". Judulbuku = "+judulbuku[i-1]+" Nama Pengarang = "+Namapengarang[i-1]+"Tahun terbit = "+TahunTerbit[i-1]);
+        }
+        System.out.println("");
     //throw new PendingException();
 }
 }
